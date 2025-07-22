872451131d2fa754b87541e92644562ee9c58b93
nadezhdaverkhovykh <verhyshka.mail@mail.ru>
2025-07-23 01:44:47 +0300

    Добавлено отключение/включение слайдера, прописан второй атрибут при переключении классов

--- js/form.js
+++ js/form.js
@@ -4,28 +4,31 @@ const formElements = document.querySelectorAll(".ad-form__element");
 const mapFiltersForm = document.querySelector(".map__filters");
 const mapFilterElements = document.querySelectorAll(".map__filter");
 const mapFeatures = document.querySelector(".map__features");
+const priceSlider = document.querySelector(".ad-form__slider");
 
 export function disableForm() {
-  form.classList.toggle("ad-form--disabled");
+  form.classList.toggle("ad-form--disabled", "");
   formHeader.setAttribute("disabled", "");
   formElements.forEach((el) => el.setAttribute("disabled", ""));
-  mapFiltersForm.classList.toggle("ad-form--disabled");
+  priceSlider.setAttribute("disabled", "");
+  mapFiltersForm.classList.toggle("ad-form--disabled", "");
   mapFilterElements.forEach((el) => el.setAttribute("disabled", ""));
   mapFeatures.setAttribute("disabled", "");
 }
 
 disableForm();
 
-export function enableForm() {
-  form.classList.toggle("ad-form--disabled");
-  formHeader.removeAttribute("disabled", "");
-  formElements.forEach((el) => el.removeAttribute("disabled", ""));
-}
-enableForm();
+// export function enableForm() {
+//   form.classList.toggle("ad-form--disabled", "");
+//   formHeader.removeAttribute("disabled", "");
+//   priceSlider.removeAttribute("disabled", "");
+//   formElements.forEach((el) => el.removeAttribute("disabled", ""));
+// }
+// enableForm();
 
-export function enableMapFilters() {
-  mapFiltersForm.classList.toggle("ad-form--disabled");
-  mapFilterElements.forEach((el) => el.removeAttribute("disabled", ""));
-  mapFeatures.removeAttribute("disabled", "");
-}
-enableMapFilters();
+// export function enableMapFilters() {
+//   mapFiltersForm.classList.toggle("ad-form--disabled", "");
+//   mapFilterElements.forEach((el) => el.removeAttribute("disabled", ""));
+//   mapFeatures.removeAttribute("disabled", "");
+// }
+// enableMapFilters();
