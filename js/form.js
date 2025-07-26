02e0031e111dd6937bb4eecaf461389c5b286ac6
nadezhdaverkhovykh <verhyshka.mail@mail.ru>
2025-07-26 23:39:53 +0300

    Валидация формы

--- js/form.js
+++ js/form.js
@@ -1,11 +1,16 @@
 const form = document.querySelector('.ad-form');
+const priceInput = form.price;
+const accomadationInput = form.type;
+const checkin = form.querySelector('[name="timein"]');
+const checkout = form.querySelector('[name="timeout"]');
+
 const formHeader = document.querySelector('.ad-form-header');
 const formElements = document.querySelectorAll('.ad-form__element');
 const mapFiltersForm = document.querySelector('.map__filters');
 const mapFilterElements = document.querySelectorAll('.map__filter');
 const mapFeatures = document.querySelector('.map__features');
 const priceSlider = document.querySelector('.ad-form__slider');
-console.log(formHeader);
+
 export function disableForm() {
   form.classList.toggle('ad-form--disabled', true);
   formHeader.setAttribute('disabled', true);
@@ -34,11 +39,12 @@ export function enableMapFilters() {
 enableMapFilters();
 
 const pristine = new Pristine(form, {
-  classTo: 'ad-form__element', // родитель, куда будет добавлен класс ошибки
-  errorClass: 'ad-form__element--invalid', // класс ошибки (уже стилизован в CSS)
-  errorTextParent: 'ad-form__element', // куда вставить текст ошибки
+  classTo: 'ad-form__element',
+  errorClass: 'ad-form__element--invalid',
+  errorTextParent: 'ad-form__element',
   errorTextClass: 'text-help',
 });
+
 
 function checkTitleHandler(value) {
   return value.length >= 30 && value.length <= 100;
@@ -46,8 +52,78 @@ function checkTitleHandler(value) {
 pristine.addValidator(
   form.querySelector('#title'),
   checkTitleHandler,
-  ' от 30 до 100'
+  ' Заголовок должен быть от 30 знаков до 100'
 );
+
+pristine.addValidator(priceInput,checkPriceHandler,'Максимальное значение — 100 000 руб.');
+
+function checkPriceHandler(value) {
+  return value <= 100000;
+}
+
+pristine.addValidator(accomadationInput, checkAccomodationHandler);
+function checkAccomodationHandler() {
+  switch (accomadationInput.value) {
+    case 'bungalow':priceInput.setAttribute('placeholder','0');
+      break;
+    case 'flat':priceInput.setAttribute('placeholder','1000');
+      break;
+    case 'hotel':
+      priceInput.setAttribute('placeholder','3000');
+      break;
+    case 'house': priceInput.setAttribute('placeholder','5000');
+      break;
+    case 'palace': priceInput.setAttribute('placeholder','10000');
+      break;
+  }
+  return accomadationInput.value;
+}
+
+pristine.addValidator(priceInput, priceForNight, 'Цена должна быть не ниже указанной');
+function priceForNight() {
+  const bungalowPrice = priceInput.value >= 0;
+  const flatPrice = priceInput.value >= 1000;
+  const hotelPrice = priceInput.value >= 3000;
+  const housePrice = priceInput.value >= 5000;
+  const palacePrice = priceInput.value >= 10000;
+  if (accomadationInput.value === 'bungalow') {
+    return bungalowPrice;
+  } else if (accomadationInput.value === 'flat') {
+    return flatPrice;
+  } else if (accomadationInput.value === 'hotel') {
+    return hotelPrice;
+  } else if (accomadationInput.value === 'house') {
+    return housePrice;
+  } else if (accomadationInput.value === 'palace') {
+    return palacePrice;
+  }
+}
+const rooms = form.rooms;
+const guests = form.capacity;
+const maxAmount = {
+  '1': ['1'],
+  '2': ['1','2'],
+  '3': ['1','2','3'],
+  '100': ['0']
+};
+function checkinCapacityHandler() {
+  const roomsAmount = rooms.value;
+  const guestsAmount = guests.value;
+  const checkAvailable = maxAmount[roomsAmount];
+  return checkAvailable.includes(guestsAmount);
+}
+
+pristine.addValidator(rooms,checkinCapacityHandler,'Количество комнат не соответсвует количеству гостей');
+pristine.addValidator(guests, checkinCapacityHandler, 'Количество гостей не соответсвует количеству комнат');
+
+function checkinTimeHandler () {
+  const checkinValue = checkin.value;
+  const checkoutValue = checkout.value;
+  return checkinValue === checkoutValue;
+}
+pristine.addValidator(checkin ,checkinTimeHandler,'Въезд невозможен');
+pristine.addValidator(checkout ,checkinTimeHandler,'Выезд невозможен');
+
 
 function setupFormValidation() {
   form.addEventListener('submit', (event) => {
@@ -61,3 +137,5 @@ function setupFormValidation() {
   });
 }
 export default setupFormValidation;
+
+
