bba9d910c5f07b1e4759f22ed501dfc0cfcacbb1
nadezhdaverkhovykh <verhyshka.mail@mail.ru>
2025-08-17 23:29:49 +0300

    Добавлен  фильтр checkbox

--- js/mapFilters.js
+++ js/mapFilters.js
@@ -1,30 +1,26 @@
 
 export function filterAdverts(adverts) {
-  const slicedAdverts = adverts.slice(0, 10);
-  let filteredAdverts = slicedAdverts;
+  // const slicedAdverts = adverts.slice(0, 10);
+  //
   const accommodationTypeSelector = document.querySelector('#housing-type');
   const accommodationPriceSelector = document.querySelector('#housing-price');
   const accommodationRoomsSelector = document.querySelector('#housing-rooms');
   const accommodationGuestsSelector = document.querySelector('#housing-guests');
   const accommodationFilter = document.querySelector('.map__filters');
+
   accommodationFilter.addEventListener('change', accommodationChangeHandler);
-  function accommodationChangeHandler(event) {
-    switch (accommodationTypeSelector.value) {
-      //   case 'any': return slicedAdverts;
-      case 'bungalow': filteredAdverts = filteredAdverts.filter((el) => el.offer.type === 'bungalow');
-        break;
-      case 'flat': filteredAdverts = filteredAdverts.filter((el) => el.offer.type === 'flat');
-        break;
-      case 'hotel': filteredAdverts = filteredAdverts.filter((el) => el.offer.type === 'hotel');
-        break;
-      case 'house': filteredAdverts = filteredAdverts.filter((el) => el.offer.type === 'house');
-        break;
-      case 'palace': filteredAdverts = filteredAdverts.filter((el) => el.offer.type === 'palace');
-        break;
-    }
+  function accommodationChangeHandler() {
+    let filteredAdverts = adverts.slice(0);
+    const accommodationFeatures = document.querySelectorAll('.map__checkbox:checked');
+    filteredAdverts = filteredAdverts.filter((el) => {
+      if (accommodationTypeSelector.value === el.offer.type) {
+        return true;
+      }
+      if (accommodationTypeSelector.value === 'any') {
+        return true;
+      }
+    });
     switch (accommodationPriceSelector.value) {
-      //   case 'any': console.log(filteredAdverts);
-      //     break;
       case 'middle': filteredAdverts = filteredAdverts = filteredAdverts.filter((el) => el.offer.price >= 10000 && el.offer.price < 50000);
         break;
       case 'low': filteredAdverts = filteredAdverts.filter((el) => el.offer.price < 10000);
@@ -32,47 +28,38 @@ export function filterAdverts(adverts) {
       case 'high': filteredAdverts = filteredAdverts.filter((el) => el.offer.price >= 50000);
         break;
     }
-    switch (accommodationRoomsSelector.value) {
-      // case 'any': console.log(slicedAdverts);
-      //   break;
-      case '1': filteredAdverts = filteredAdverts.filter((el) => el.offer.rooms === 1);
-        break;
-      case '2': filteredAdverts = filteredAdverts.filter((el) => el.offer.rooms === 2);
-        break;
-      case '3': filteredAdverts = filteredAdverts.filter((el) => el.offer.rooms === 3);
-        break;
-    }
-    switch (accommodationGuestsSelector.value) {
-      // case 'any': console.log(slicedAdverts);
-      //   break;
-      case '2': console.log(filteredAdverts = filteredAdverts.filter((el) => el.offer.guests === 2));
-        break;
-      case '1': console.log(filteredAdverts = filteredAdverts.filter((el) => el.offer.guests === 1));
-        break;
-      case '0': console.log(filteredAdverts = filteredAdverts.filter((el) => el.offer.guests === 0));
-        break;
-    }
-    if (event.target.checked === true) {
-      filteredAdverts = filteredAdverts.filter((el) => el.offer.features);
-      for (const key of filteredAdverts) {
-        const keys = key.offer.features;
-        console.log(keys.filter((el) => el.includes(event.target.value)));
+    filteredAdverts = filteredAdverts.filter((el) => {
+      if (Number(accommodationRoomsSelector.value) === el.offer.rooms) {
+        return true;
       }
+      if (accommodationRoomsSelector.value === 'any') {
+        return true;
+      }
+    });
+    filteredAdverts = filteredAdverts.filter((el) => {
+      if (Number(accommodationGuestsSelector.value) === el.offer.guests) {
+        return true;
+      }
+      if (accommodationGuestsSelector.value === 'any') {
+        return true;
+      }
+    });
+
+    const featuresArray = [...accommodationFeatures];
+
+    if (accommodationFeatures.length >= 1) {
+      const featuresStringArr = featuresArray.map((featureInput) => featureInput.value
+      );
+      filteredAdverts = filteredAdverts.filter((advert) => {
+        if (!advert.offer.features) {
+          return false;
+        }
+
+        return featuresStringArr.every((featuresString) => advert.offer.features.includes(featuresString));
+      });
+
+
     }
-
-
-    //   //По удобствам
-
-    //   const accommodationFeatures = document.querySelector('#housing-features');
-    //   accommodationFeatures.addEventListener('change', accommodationFeaturesChangeHandler);
-    //   function accommodationFeaturesChangeHandler(event) {
-    //     if (event.target.checked === true) {
-    //       const features = slicedAdverts.filter((el) => el.offer.features) ;
-    //       for (const key of features) {
-    //         const keys = key.offer.features;
-    //         console.log(keys.filter((el)=> el.includes(event.target.value))) ;
-    //       }
-    //     }
-    //   }
+    console.log(filteredAdverts);
   }
 }
